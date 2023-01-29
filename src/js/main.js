efcd7505477e9224ab896e01d648e46e473df479
SadeghAT <sadegh.artivox@gmail.com>
2023-01-29 23:48:37 +0330

    start coding js

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// CommonJS
+const Swal = require("sweetalert2");
+Swal.fire({
+  title: "Error!",
+  text: "Do you want to continue",
+  icon: "error",
+  confirmButtonText: "Cool",
+});
+
+console.log("sadasd");
